c39c4527959fa28540294567f60bae5385f9b247
Paweł Wilkosz <pwilkosz5@gmail.com>
2019-11-24 01:59:31 +0100

    I'm sure it would be easier to write normal commit messages than to think about this stupid introspecitve fourth wall breaking shit but, well, I have to be consistent with my own standards. Have a nice day/night/life/whatever you have.

--- l3/code/zad5.jl
+++ l3/code/zad5.jl
@@ -5,4 +5,4 @@ using .MiejscaZerowe
 f(x::Float64) = 3*x - exp(x)
 
 println(mbisekcji(f, 0.0, 1.0, 10^(-4), 10^(-4)))
-println(mbisekcji(f, 1.1, 5.0, 10^(-4), 10^(-4)))
+println(mbisekcji(f, 1.0, 10.0, 10^(-4), 10^(-4)))
--- l3/code/zad6.jl
+++ l3/code/zad6.jl
@@ -2,7 +2,7 @@
 include("./MiejscaZerowe.jl")
 using .MiejscaZerowe
 
-f1(x::Float64) = exp(1-x)
+f1(x::Float64) = exp(1-x) - 1
 f1p(x::Float64) = -exp(1-x)
 f2(x::Float64) = x*exp(-x)
 f2p(x::Float64) = -(x-1)*exp(-x)
@@ -10,15 +10,16 @@ f2p(x::Float64) = -(x-1)*exp(-x)
 epsilon = 10^(-5)
 delta = 10^(-5)
 
-println(mbisekcji(f1, 0.0, 2.0, delta, epsilon))
-println(mbisekcji(f2, -1.0, 1.0, delta, epsilon))
-
+println(mbisekcji(f1, 0.0, 10.0, delta, epsilon))
 println(mstycznych(f1, f1p, 0.0, delta, epsilon, 100))
-println(mstycznych(f2, f2p, -1.0, delta, epsilon, 100))
-
 println(msiecznych(f1, 0.0, 0.1, delta, epsilon, 100))
-println(msiecznych(f2, -1.0, -0.9, delta, epsilon, 100))
 
-println(mstycznych(f1, f1p, 100.0, delta, epsilon, 100))
-println(mstycznych(f2, f2p, 100.0, delta, epsilon, 100))
+
+println(mbisekcji(f2, -1.0, 1.0, delta, epsilon))
+println(mstycznych(f2, f2p, -1.0, delta, epsilon, 100))
+println(msiecznych(f2, -1.0, -0.5, delta, epsilon, 100))
+
+
+println(mstycznych(f1, f1p, 50.0, delta, epsilon, 100))
+println(mstycznych(f2, f2p, 50.0, delta, epsilon, 100))
 println(mstycznych(f2, f2p, 1.0, delta, epsilon, 100))
